f6deb60b8c335c6bc01452dadbb3e5f6eee4e28a
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2024-09-06 07:26:46 +0600

    2done

---
+++ 1.cpp
@@ -0,0 +1,8 @@
+
+/*
+Object-Oriented Programming (OOP) is a programming model that organizes software design around objects, which have attributes (data) and behavior (methods). It focuses on manipulating objects rather than just logic. OOP is ideal for large, complex, and maintainable software, promoting code reusability, scalability, and efficiency. It also supports collaborative development by dividing projects into manageable parts.
+
+follow below link for difference between procedural and oop ?
+https://www.geeksforgeeks.org/differences-between-procedural-and-object-oriented-programming/
+
+*/
